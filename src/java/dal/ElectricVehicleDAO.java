d101ea1a08f82ee22690abf748f30a2550bc3b4f
TAnhpika <anhsheeran12345@gmail.com>
2026-03-18 21:14:47 +0700

    DAO format

--- src/java/dal/ElectricVehicleDAO.java
+++ src/java/dal/ElectricVehicleDAO.java
@@ -17,8 +17,7 @@ public class ElectricVehicleDAO extends DBContext {
 
     public void addVehicle(ElectricVehicle ev) {
         String sql = "INSERT INTO [dbo].[ElectricVehicle] ([VehicleID], [ModelName], [Price], [BatteryType]) VALUES (?, ?, ?, ?)";
-        try {
-            PreparedStatement st = c.prepareStatement(sql);
+        try (PreparedStatement st = c.prepareStatement(sql)) {
             st.setString(1, ev.getVehicleID());
             st.setString(2, ev.getModelName());
             st.setDouble(3, ev.getPrice());
@@ -32,9 +31,8 @@ public class ElectricVehicleDAO extends DBContext {
     public List<ElectricVehicle> listAllVehicles() {
         List<ElectricVehicle> list = new ArrayList<>();
         String sql = "SELECT * FROM ElectricVehicle";
-        try {
-            PreparedStatement st = c.prepareStatement(sql);
-            ResultSet rs = st.executeQuery();
+        try (PreparedStatement st = c.prepareStatement(sql);
+             ResultSet rs = st.executeQuery()) {
             while (rs.next()) {
                 list.add(new ElectricVehicle(
                         rs.getString("VehicleID"),
@@ -52,17 +50,17 @@ public class ElectricVehicleDAO extends DBContext {
     public List<ElectricVehicle> searchByBatteryType(String batteryType) {
         List<ElectricVehicle> list = new ArrayList<>();
         String sql = "SELECT * FROM ElectricVehicle WHERE BatteryType LIKE ?";
-        try {
-            PreparedStatement st = c.prepareStatement(sql);
+        try (PreparedStatement st = c.prepareStatement(sql)) {
             st.setString(1, "%" + batteryType + "%");
-            ResultSet rs = st.executeQuery();
-            while (rs.next()) {
-                list.add(new ElectricVehicle(
-                        rs.getString("VehicleID"),
-                        rs.getString("ModelName"),
-                        rs.getDouble("Price"),
-                        rs.getString("BatteryType")
-                ));
+            try (ResultSet rs = st.executeQuery()) {
+                while (rs.next()) {
+                    list.add(new ElectricVehicle(
+                            rs.getString("VehicleID"),
+                            rs.getString("ModelName"),
+                            rs.getDouble("Price"),
+                            rs.getString("BatteryType")
+                    ));
+                }
             }
         } catch (SQLException ex) {
             Logger.getLogger(ElectricVehicleDAO.class.getName()).log(Level.SEVERE, null, ex);
@@ -72,8 +70,7 @@ public class ElectricVehicleDAO extends DBContext {
 
     public void deleteVehicle(String vehicleID) {
         String sql = "DELETE FROM [dbo].[ElectricVehicle] WHERE [VehicleID] = ?";
-        try {
-            PreparedStatement st = c.prepareStatement(sql);
+        try (PreparedStatement st = c.prepareStatement(sql)) {
             st.setString(1, vehicleID);
             st.executeUpdate();
         } catch (SQLException ex) {
@@ -83,17 +80,17 @@ public class ElectricVehicleDAO extends DBContext {
 
     public ElectricVehicle getVehicleById(String vehicleID) {
         String sql = "SELECT * FROM [dbo].[ElectricVehicle] WHERE [VehicleID] = ?";
-        try {
-            PreparedStatement st = c.prepareStatement(sql);
+        try (PreparedStatement st = c.prepareStatement(sql)) {
             st.setString(1, vehicleID);
-            ResultSet rs = st.executeQuery();
-            if (rs.next()) {
-                return new ElectricVehicle(
-                        rs.getString("VehicleID"),
-                        rs.getString("ModelName"),
-                        rs.getDouble("Price"),
-                        rs.getString("BatteryType")
-                );
+            try (ResultSet rs = st.executeQuery()) {
+                if (rs.next()) {
+                    return new ElectricVehicle(
+                            rs.getString("VehicleID"),
+                            rs.getString("ModelName"),
+                            rs.getDouble("Price"),
+                            rs.getString("BatteryType")
+                    );
+                }
             }
         } catch (SQLException ex) {
             Logger.getLogger(ElectricVehicleDAO.class.getName()).log(Level.SEVERE, null, ex);
@@ -103,8 +100,7 @@ public class ElectricVehicleDAO extends DBContext {
 
     public void updateVehicle(ElectricVehicle ev) {
         String sql = "UPDATE [dbo].[ElectricVehicle] SET [ModelName] = ?, [Price] = ?, [BatteryType] = ? WHERE [VehicleID] = ?";
-        try {
-            PreparedStatement st = c.prepareStatement(sql);
+        try (PreparedStatement st = c.prepareStatement(sql)) {
             st.setString(1, ev.getModelName());
             st.setDouble(2, ev.getPrice());
             st.setString(3, ev.getBatteryType());
